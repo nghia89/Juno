a2b53c58ffafe5a704e10a8d334aa5115d3bd296
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-04-14 13:40:18 +0700

    Add model

---
+++ Juno.Model/Models/VisitorStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juno.Model.Models
+{
+    [Table("VisitorStatistics")]
+    public class VisitorStatistic //thống kê truy cập
+    {
+        [Key]
+        public Guid ID { set; get; }
+
+        [Required]
+        public DateTime VisitedDate { set; get; }
+
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
