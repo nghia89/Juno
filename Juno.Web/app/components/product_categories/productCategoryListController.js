b5469215f6699520a4fe811f84e66fe1b264be12
DESKTOP-O9PNQI9\thanh <hangnghia11089@gmail.com>
2018-04-23 23:21:26 +0700

    tạo notification product list view angular

--- Juno.Web/app/components/product_categories/productCategoryListController.js
+++ Juno.Web/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService']
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService']
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -23,6 +23,11 @@
                 }
             }
             apiService.get('/Api/ProductCategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('không có bản ghi nào được tìm thấy.');
+                } else {
+                    notificationService.displaySuccess('Đã tìm thấy' +' '+ result.data.TotalCount+' '+ 'bản ghi')
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
